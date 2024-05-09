9fecaf4842671afcc513592a69c6f4bfb8b16236
Aetias <aetias@outlook.com>
2024-05-09 14:54:40 +0200

    Implement opcode tree search

--- generator/src/main.rs
+++ generator/src/main.rs
@@ -11,17 +11,14 @@ use isa::Isa;
 
 use crate::{disasm::generate_disasm, search::SearchTree};
 
-/// These bits are always defined in every ARM mnemonic, and all other bits vary.
-const BUCKET_BITMASK: u32 = 0x0c000000;
-
 fn main() -> Result<()> {
     let arm = Isa::load(Path::new("arm.yaml"))?;
     arm.validate()?;
     println!("{} opcodes", arm.opcodes.len());
 
-    // let tree = SearchTree::optimize(arm.opcodes.to_vec());
+    // let tree = SearchTree::optimize(&arm.opcodes.to_vec());
 
-    let tokens = generate_disasm(&arm, BUCKET_BITMASK).context("While generating tokens for disassembler")?;
+    let tokens = generate_disasm(&arm).context("While generating tokens for disassembler")?;
     let file = syn::parse2(tokens).context("While parsing tokens for disassembler")?;
     let formatted = prettyplease::unparse(&file);
     fs::write("disasm/src/generated.rs", formatted)?;
--- generator/src/search.rs
+++ generator/src/search.rs
@@ -1,27 +1,27 @@
 use crate::isa::Opcode;
 
 pub struct SearchTree {
-    bitmask: u32,
-    left_pattern: u32,
-    left: Option<Box<SearchTree>>,
-    right: Option<Box<SearchTree>>,
+    pub bitmask: u32,
+    pub left_pattern: u32,
+    pub left: Option<Box<SearchTree>>,
+    pub right: Option<Box<SearchTree>>,
 }
 
 impl SearchTree {
-    fn narrow_down(opcodes: &[Opcode]) -> Self {
+    fn narrow_down(opcodes: &[Opcode]) -> Option<Self> {
         // Goal: Get this number as close to opcodes.len() / 2 as possible
-        let mut best_narrow_count = 0;
-
+        let mut best_narrow_count = opcodes.len();
         let mut best_bitmask = 0;
         let mut best_left_pattern = 0;
 
         for pos in 0..32 {
             let bitmask = 1 << pos;
 
+            let unks = opcodes.iter().filter(|op| (op.bitmask & bitmask) != bitmask).count();
             let (zeros, ones) = opcodes
                 .iter()
                 .filter(|op| (op.bitmask & bitmask) == bitmask)
-                .fold((0, 0), |acc, op| {
+                .fold((unks, unks), |acc, op| {
                     if op.pattern & bitmask == 0 {
                         (acc.0 + 1, acc.1)
                     } else {
@@ -29,48 +29,78 @@ impl SearchTree {
                     }
                 });
 
-            if zeros > best_narrow_count && zeros <= opcodes.len() / 2 {
+            if zeros > unks && zeros < best_narrow_count && zeros >= opcodes.len() / 2 {
                 best_narrow_count = zeros;
                 best_bitmask = bitmask;
                 best_left_pattern = 0;
             }
-            if ones > best_narrow_count && ones <= opcodes.len() / 2 {
+            if ones > unks && ones < best_narrow_count && ones >= opcodes.len() / 2 {
                 best_narrow_count = ones;
                 best_bitmask = bitmask;
                 best_left_pattern = bitmask;
             }
         }
 
-        Self {
-            bitmask: best_bitmask,
-            left_pattern: best_left_pattern,
-            left: None,
-            right: None,
+        if best_narrow_count == opcodes.len() {
+            // Impossible to narrow down
+            None
+        } else {
+            Some(Self {
+                bitmask: best_bitmask,
+                left_pattern: best_left_pattern,
+                left: None,
+                right: None,
+            })
         }
     }
 
-    pub fn filter(&self, opcodes: Vec<Opcode>) -> (Vec<Opcode>, Vec<Opcode>) {
+    pub fn filter(&self, opcodes: &Vec<Opcode>) -> (Vec<Opcode>, Vec<Opcode>) {
         let mut left = vec![];
         let mut right = vec![];
         for opcode in opcodes {
-            if (opcode.bitmask & self.bitmask) != 0 && (opcode.pattern & self.bitmask) == self.left_pattern {
-                left.push(opcode);
+            if (opcode.bitmask & self.bitmask) == 0 {
+                left.push(opcode.clone());
+                right.push(opcode.clone());
+            } else if (opcode.pattern & self.bitmask) == self.left_pattern {
+                left.push(opcode.clone());
             } else {
-                right.push(opcode);
+                right.push(opcode.clone());
             }
         }
         (left, right)
     }
 
-    pub fn optimize(opcodes: Vec<Opcode>) -> Self {
-        let mut node = Self::narrow_down(&opcodes);
-        let (left, right) = node.filter(opcodes);
-        if left.len() > 1 {
-            node.left = Some(Box::new(Self::optimize(left)));
+    pub fn optimize(opcodes: &Vec<Opcode>, max_depth: u32) -> Option<Self> {
+        if max_depth == 0 {
+            return None;
         }
-        if right.len() > 1 {
-            node.right = Some(Box::new(Self::optimize(right)));
+
+        let node = Self::narrow_down(opcodes);
+        // println!("{{");
+        if let Some(mut node) = node {
+            // println!("\"size\":{}", opcodes.len());
+            let (left, right) = node.filter(opcodes);
+            if left.len() > 1 {
+                // println!(",\"left\":");
+                if let Some(left) = Self::optimize(&left, max_depth - 1) {
+                    node.left = Some(Box::new(left));
+                } else {
+                    // println!("\"opcode {}\"", left[0].name);
+                }
+            }
+            if right.len() > 1 {
+                // println!(",\"right\":");
+                if let Some(right) = Self::optimize(&right, max_depth - 1) {
+                    node.right = Some(Box::new(right));
+                } else {
+                    // println!("\"opcode {}\"", right[0].name);
+                }
+            }
+            // println!("}}");
+            Some(node)
+        } else {
+            // println!("}}");
+            None
         }
-        node
     }
 }
