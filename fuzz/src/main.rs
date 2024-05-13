b22132a3a3afd7ae11a5662d6cb59f311772541b
Aetias <aetias@outlook.com>
2024-05-13 19:14:57 +0200

    Add fuzzer for Thumb

--- fuzz/src/main.rs
+++ fuzz/src/main.rs
@@ -1,47 +1,44 @@
-use std::{hint::black_box, ops::RangeInclusive, time::Instant};
+mod arm;
+mod thumb;
 
-use armv5te::arm;
+use std::time::Instant;
 
 fn main() {
-    let threads = num_cpus::get();
-    println!("Starting {} threads", threads);
-    let start = Instant::now();
-    fuzz(threads);
-    println!("Finished in {:.2}s", start.elapsed().as_secs_f32());
-}
-
-fn fuzz(num_threads: usize) {
-    let fuzzers: Vec<_> = (0..num_threads)
-        .map(|i| {
-            let start = ((0x100000000 * i) / num_threads).try_into().unwrap();
-            let end = ((0x100000000 * (i + 1)) / num_threads - 1).try_into().unwrap();
-            Fuzzer::new(start..=end)
-        })
-        .collect();
-
-    let handles: Vec<_> = fuzzers.iter().map(|f| f.run()).collect();
-    for handle in handles {
-        handle.join().unwrap();
-    }
-}
-
-struct Fuzzer {
-    range: RangeInclusive<u32>,
-}
-
-impl Fuzzer {
-    fn new(range: RangeInclusive<u32>) -> Self {
-        Self { range }
-    }
-
-    fn run(&self) -> std::thread::JoinHandle<()> {
-        let range = self.range.clone();
-        std::thread::spawn(move || {
-            let mut parsed = arm::ParsedIns::default();
-            for code in range {
-                #[allow(clippy::unit_arg)]
-                black_box(arm::parse(&mut parsed, arm::Ins::new(code)));
+    let (threads, iterations, arm, thumb) = {
+        let mut threads = num_cpus::get();
+        let mut iterations = 1;
+        let mut arm = false;
+        let mut thumb = false;
+        let mut args = std::env::args();
+        args.next(); // skip program name
+        while let Some(arg) = args.next() {
+            match arg.as_str() {
+                "-t" => threads = args.next().and_then(|a| a.parse().ok()).expect("Expected number after -t"),
+                "-n" => iterations = args.next().and_then(|a| a.parse().ok()).expect("Expected number after -n"),
+                "arm" => arm = true,
+                "thumb" => thumb = true,
+                _ => panic!("Unknown argument '{}'", arg),
             }
-        })
+        }
+        (threads, iterations, arm, thumb)
+    };
+    if threads == 0 {
+        panic!("Number of threads must be positive");
     }
+    if iterations == 0 {
+        panic!("Number of iterations must be positive");
+    }
+    if arm == thumb {
+        panic!("Expected one of 'arm' and 'thumb'");
+    }
+
+    println!("Starting {} threads running {} iterations", threads, iterations);
+    let start = Instant::now();
+    if arm {
+        arm::fuzz(threads, iterations);
+    }
+    if thumb {
+        thumb::fuzz(threads, iterations);
+    }
+    println!("Finished in {:.2}s", start.elapsed().as_secs_f32());
 }
