1de9778ec49d860b9d63b6581126891dc7826919
Aetias <aetias@outlook.com>
2025-09-28 01:04:17 +0200

    Add `StringFormatter` for formatting instructions to a String

--- disasm/src/fmt.rs
+++ disasm/src/fmt.rs
@@ -1,3 +1,5 @@
+use alloc::string::String;
+
 use crate::{Ins, Options, Write};
 
 pub struct Formatter<'a, 'b> {
@@ -30,5 +32,33 @@ impl<'a> core::fmt::Display for DisplayIns<'a> {
     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
         let mut formatter = Formatter { options: self.options, formatter: f };
         formatter.write_ins(self.ins)
+    }
+}
+
+pub struct StringFormatter<'a> {
+    pub options: &'a Options,
+    string: String,
+}
+
+impl<'a> StringFormatter<'a> {
+    pub fn new(options: &'a Options) -> Self {
+        Self { options, string: String::new() }
+    }
+
+    pub fn into_string(self) -> String {
+        self.string
+    }
+}
+
+impl core::fmt::Write for StringFormatter<'_> {
+    fn write_str(&mut self, s: &str) -> core::fmt::Result {
+        self.string.push_str(s);
+        Ok(())
+    }
+}
+
+impl Write for StringFormatter<'_> {
+    fn options(&self) -> &Options {
+        self.options
     }
 }
