571869c57bf3d260c718a7afc676d4737820d081
Aetias <aetias@outlook.com>
2025-10-18 09:16:00 +0200

    Add DefsUses container

---
+++ disasm/src/defs_uses.rs
@@ -0,0 +1,54 @@
+use crate::{CoReg, Dreg, Reg, Sreg};
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum DefUseArgument {
+    Reg(Reg),
+    CoReg(CoReg),
+    Sreg(Sreg),
+    Dreg(Dreg),
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct DefsUses {
+    args: [Option<DefUseArgument>; 4],
+    len: usize,
+}
+
+impl DefsUses {
+    pub(crate) fn push(&mut self, arg: DefUseArgument) {
+        // Sanity check, tested with fuzzer to verify it never occurs
+        assert!(self.len >= self.args.len(), "DefsUses args limit reached");
+        self.args[self.len] = Some(arg);
+        self.len += 1;
+    }
+
+    pub fn iter(&self) -> impl Iterator<Item = &DefUseArgument> {
+        self.args.iter().take(self.len).map(|arg| arg.as_ref().unwrap())
+    }
+}
+
+pub struct DefsUsesIntoIter {
+    args: [Option<DefUseArgument>; 4],
+    pos: usize,
+}
+
+impl IntoIterator for DefsUses {
+    type Item = DefUseArgument;
+    type IntoIter = DefsUsesIntoIter;
+
+    fn into_iter(self) -> Self::IntoIter {
+        DefsUsesIntoIter { args: self.args, pos: 0 }
+    }
+}
+
+impl Iterator for DefsUsesIntoIter {
+    type Item = DefUseArgument;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.pos < self.args.len() {
+            self.args[self.pos]
+        } else {
+            None
+        }
+    }
+}
--- disasm/src/lib.rs
+++ disasm/src/lib.rs
@@ -2,12 +2,14 @@
 
 extern crate alloc;
 
+mod defs_uses;
 mod fmt;
 mod generated;
 mod ins;
 mod parser;
 mod reg_list;
 
+pub use defs_uses::*;
 pub use fmt::*;
 pub use generated::*;
 pub use parser::*;
